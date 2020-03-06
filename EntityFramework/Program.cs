46ef0da6c4e7643b1eceb55e3fe2fe3ebc654b97
Jowendean <jowendean1@gmail.com>
2020-03-06 17:11:34 -0600

    Add project files.

---
+++ EntityFramework/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new EfExampleEntities())
+            {
+                Console.WriteLine("All Students");
+                var allstudents = db.Students.Select(s => s.Name);
+
+                foreach(var v in allstudents)
+                {
+                    Console.WriteLine(v);
+                }
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
